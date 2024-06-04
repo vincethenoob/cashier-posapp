ac6e74255c559d4f38a5832f14f6d4971763849a
PR1VATES <125135312+PR1VATES@users.noreply.github.com>
2024-06-04 11:39:47 +0800

    Delete cashier-posapp directory

--- cashier-posapp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module guis.cashierposapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-    opens guis.cashierposapp to javafx.fxml, javafx.graphics;
-    exports guis.cashierposapp;
-}
